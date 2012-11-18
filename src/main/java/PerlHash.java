b269a23fa9b3ce1928dc3184f1c794673cfc85fc
Mohamed Ibrahim <mibrahim@mibrahim.net>
2012-11-18 12:29:00 -0500

    Simple speed comparison between Perl hash and Murmur hash in Java, from jruby.

--- src/main/java/PerlHash.java
+++ src/main/java/PerlHash.java
@@ -1,9 +1,22 @@
+
 /**
- * Created with IntelliJ IDEA.
- * User: mosama
- * Date: 11/18/12
- * Time: 12:00 PM
- * To change this template use File | Settings | File Templates.
+ * Perl's Hash implementation.
+ *
+ * @author nahi@ruby-lang.org
+ *
+ * Downloaded from: https://github.com/jruby/jruby/blob/5e4aab28b26fd127112b76fabfac9a33b64caf77/src/org/jruby/util/PerlHash.java
+ *
  */
 public class PerlHash {
+    public static long hash(long key, byte[] src, int offset, int length) {
+        for (int idx = 0; idx < length; ++idx) {
+            key += (src[offset + idx] & 0xFF);
+            key += (key << 10);
+            key ^= (key >>> 6);
+        }
+        key += (key << 3);
+        key ^= (key >>> 11);
+        key += (key << 15);
+        return key;
+    }
 }
--- src/main/java/SpeedTest.java
+++ src/main/java/SpeedTest.java
@@ -6,4 +6,23 @@
  * To change this template use File | Settings | File Templates.
  */
 public class SpeedTest {
+
+    public static void main(String args[]){
+        byte[] blockIn = new byte[256 * 1024];
+
+        long startTime = System.currentTimeMillis();
+        int iterations=10000;
+        for (int i = 0; i != iterations; i++) {
+            MurmurHash.hash32(blockIn, 0, blockIn.length, 0);
+        }
+        long Time= System.currentTimeMillis()-startTime;
+        System.out.println("Murmur hash speed: "+(1000.0*blockIn.length*iterations/(1e6*Time))+ " MB/Sec");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i != iterations; i++) {
+            PerlHash.hash(0,blockIn,0,blockIn.length);
+        }
+        Time= System.currentTimeMillis()-startTime;
+        System.out.println("Perl hash speed: "+(1000.0*blockIn.length*iterations/(1e6*Time))+ " MB/Sec");
+    }
 }
