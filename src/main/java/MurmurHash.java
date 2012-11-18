8ec0bbf244232f99fc7ca8c303a2adaf4fc40a2c
Mohamed Ibrahim <mibrahim@mibrahim.net>
2012-11-18 12:17:33 -0500

    Simple speed comparison between Perl hash and Murmur hash in Java, from jruby.

---
+++ src/main/java/MurmurHash.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mosama
+ * Date: 11/18/12
+ * Time: 12:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MurmurHash {
+}
---
+++ src/main/java/PerlHash.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mosama
+ * Date: 11/18/12
+ * Time: 12:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PerlHash {
+}
